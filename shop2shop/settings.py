b35883f9b547e268fd2f82684febeef118b4d35c
shop2world <info@shop2world.com>
2021-08-16 18:47:54 -0500

    Update settings.py

--- shop2shop/settings.py
+++ shop2shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tuxnt%aq)6ydvwu&$x51(2xzfa*)z%3zy)9fo=nolk&=vo2b!k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
